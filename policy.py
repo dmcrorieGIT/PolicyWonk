9dd78197bca8fd1f17d37161f85f02a78e487e41
Michael Sandare <sandare@ualberta.ca>
2020-06-07 01:38:05 -0600

    policy.py added

---
+++ policy.py
@@ -0,0 +1,31 @@
+class StatusQuo:
+    raceDict = {
+        "White":{},
+        "Hispanic":{},
+        "Black":{},
+        "Asian":{},
+        "Native American":{},
+        "Native Hawaiian":{},
+        "Two or More":{},
+        "Other":{}
+    }
+
+    sexDict = {
+        "Male":{},
+        "Female":{}
+    }
+
+    sexualOrientDict = {
+        "LGBTQ+": {},
+        "Non-LGBTQ+":{}
+    }
+
+    socialClassDict = {
+        "Underclass": {},
+        "Working Poor": {},
+        "Working": {},
+        "Lower Middle": {},
+        "Upper Middle": {},
+        "Lower Upper": {},
+        "Upper Upper": {}
+    }
