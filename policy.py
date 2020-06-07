0221ed85a484f9191ef55c0ceee4d5dca6d681e1
Michael Sandare <sandare@ualberta.ca>
2020-06-07 02:36:30 -0600

    Policy.py value dictionary

--- policy.py
+++ policy.py
@@ -1,31 +1,140 @@
 class StatusQuo:
     raceDict = {
-        "White":{},
-        "Hispanic":{},
-        "Black":{},
-        "Asian":{},
-        "Native American":{},
-        "Native Hawaiian":{},
-        "Two or More":{},
-        "Other":{}
+        "White":{
+            "Illness": 0.0075,
+            "Depression":.0204,
+            "Education": 0.0039,
+            "Law": 0.00450
+        },
+        "Hispanic":{
+            "Illness": 0.0190,
+            "Depression": .0152,
+            "Education": 0.0065,
+            "Law": 0.00831
+        },
+        "Black":{
+            "Illness": 0.0115,
+            "Depression": .0162,
+            "Education": 0.0055,
+            "Law": 0.02306
+        },
+        "Asian":{
+            "Illness": 0.0068,
+            "Depression": .0145,
+            "Education": 0.0047,
+            "Law": 0.00115
+            },
+        "Native American":{
+            "Illness": 0.0218,
+            "Depression": .0189,
+            "Education": 0.0044,
+            "Law": 0.01291
+            },
+        "Native Hawaiian":{
+            "Illness": 0.0093,
+            "Depression": .0194,
+            "Education": 0.0044,
+            "Law": 0.01017
+            },
+        "Two or More":{
+            "Illness": 0.0080,
+            "Depression": .0286,
+            "Education": 0.0050,
+            "Law": 0.00831
+            }
+        # ,"Other":{
+        #     "Illness": #,
+        #     "Depression":#,
+        #     "Education":#,
+        #     "Law":#
+        #     }
     }
 
     sexDict = {
-        "Male":{},
-        "Female":{}
+        "Male":{
+            "Illness": 0.00459,
+            "Depression": .0151,
+            "Education": 0.0070,
+            "Law": 0.0054
+            },
+        "Female":{
+            "Illness": 0.00391,
+            "Depression":.0223,
+            "Education": 0.0050,
+            "Law": 0.0046
+            }
     }
 
     sexualOrientDict = {
-        "LGBTQ+": {},
-        "Non-LGBTQ+":{}
+        "LGBTQ+": {
+            "Illness":0.0050,
+            "Depression": 0.039,
+            "Education": 0.0062,
+            "Law": 0.0025
+        },
+        "Non-LGBTQ+":{
+            "Illness":0.0025,
+            "Depression": 0.0167,
+            "Education": 0.0022,
+            "Law": 0.0023
+        }
     }
 
     socialClassDict = {
-        "Underclass": {},
-        "Working Poor": {},
-        "Working": {},
-        "Lower Middle": {},
-        "Upper Middle": {},
-        "Lower Upper": {},
-        "Upper Upper": {}
+        "Underclass": {
+            "Illness": 0.0378,
+            "Depression": 0.0087,
+            #"Education":#,
+            "Law": 0.00518,
+            #"Residual Wealth":#
+            "Taxes": 0.10
+            },
+        "Working Poor": {
+            "Illness": 0.0332,
+            "Depression": 0.0087,
+            #"Education":#,
+            "Law":0.00572,
+            #"Residual Wealth":#
+            "Taxes": 0.12
+            },
+        "Working": {
+            "Illness": 0.0166,
+            "Depression": 0.0051,
+            #"Education":#,
+            "Law":0.00540,
+            #"Residual Wealth":# 
+            "Taxes": 0.17
+        },
+        "Lower Middle": {
+            "Illness": 0.0080,
+            "Depression": 0.0027,
+            #"Education":#,
+            "Law":0.00320,
+            #"Residual Wealth":#,
+            "Taxes": 0.23
+        },
+        "Upper Middle": {
+            "Illness":0.0050,
+            "Depression": 0.0027,
+            #"Education":#,
+            "Law": 0.0011,
+            #"Residual Wealth":#,
+            "Taxes": 0.27
+        },
+        "Lower Upper": {
+            "Illness": 0,
+            "Depression":0.0012,
+            #"Education":#,
+            "Law": -0.0030,
+            #"Residual Wealth":#,
+            "Taxes":0.37
+        },
+        "Upper Upper": {
+            "Illness":0,
+            "Depression": 0.0012,
+            #"Education":#,
+            "Law":-0.0050,
+            #"Residual Wealth":#,
+            "Taxes": 0.37
+        }
     }
